1fcbe7aa927b88d3e23c9aff7d9a38e4fc09885c
Alejandro Tovar <da.tovar10@uniandes.edu.co>
2023-10-02 13:35:57 -0500

    Initial Commit

---
+++ SportPal-iOS/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SportPal-iOS
+//
+//  Created by Alejandro Tovar on 2/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SportPal-iOS/SportPal_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportPal_iOSApp.swift
+//  SportPal-iOS
+//
+//  Created by Alejandro Tovar on 2/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SportPal_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
