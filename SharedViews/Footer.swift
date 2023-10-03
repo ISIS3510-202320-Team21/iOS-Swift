a5583a99c4f2c8c54acee0f4c748fbeab42dc5f0
Alejandro Tovar <da.tovar10@uniandes.edu.co>
2023-10-03 18:23:10 -0500

    Added Landing and profile views

--- SharedViews/Footer.swift
+++ SharedViews/Footer.swift
@@ -8,28 +8,31 @@
 import SwiftUI
 
 struct Footer: View {
+    @State private var isProfile: Bool = false
+
     var body: some View {
-        HStack() {
-            Button(action: {
+            HStack() {
+                Button(action: {
+                    
+                }) {
+                    Image("HomeButton")
+                }.padding()
+                Spacer()
+                Button(action: {
+                    
+                }) {
+                    Image("NewMatchButton")
+                }.padding()
+                Spacer()
+                Button(action: {
+                    isProfile=true
+                }) {
+                    Image("ProfileButton")
+                }.padding()
                 
-            }) {
-                Image("HomeButton")
-            }.padding()
-            Spacer()
-            Button(action: {
-                
-            }) {
-                Image("NewMatchButton")
-            }.padding()
-            Spacer()
-            Button(action: {
-                
-            }) {
-                Image("ProfileButton")
-            }.padding()
-            
-        }.background(Color.white)
+            }.background(Color.white)
     }
+
 }
 
 struct Footer_Previews: PreviewProvider {
