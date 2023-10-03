187a804b4402c31ef00b9b1a35370eea9e10effc
Alejandro Tovar <da.tovar10@uniandes.edu.co>
2023-10-03 12:17:45 -0500

    Added general footer

---
+++ SharedViews/Footer.swift
@@ -0,0 +1,20 @@
+//
+//  Footer.swift
+//  SportPal-iOS
+//
+//  Created by Alejandro Tovar on 3/10/23.
+//
+
+import SwiftUI
+
+struct Footer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Footer_Previews: PreviewProvider {
+    static var previews: some View {
+        Footer()
+    }
+}
