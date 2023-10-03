61a025e189fb1c86dfc7a1eb0c5d3b71c8a75f32
Alejandro Tovar <da.tovar10@uniandes.edu.co>
2023-10-03 10:28:36 -0500

    Login view and shared views created

---
+++ SharedViews/Header.swift
@@ -0,0 +1,42 @@
+//
+//  Header.swift
+//  SportPal-iOS
+//
+//  Created by Alejandro Tovar on 3/10/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct HeaderView: View {
+    
+    var title: String
+    var notifications: Bool
+    var messages: Bool
+    
+    var body: some View {
+        HStack() {
+            Text(title)
+                .font(.title)
+            Spacer()
+            if(messages) {
+                //TODO: Connect to message view
+                Button(action:{}) {
+                    Image("MessagesIcon")
+                }
+            }
+            if(notifications) {
+                //TODO: Connect to notifications view
+                Button(action:{}) {
+                    Image("NotificationsIcon")
+                }
+            }
+        }.padding()
+    }
+}
+
+struct HeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        HeaderView(title: "LOGIN", notifications: true, messages: true)
+    }
+}
