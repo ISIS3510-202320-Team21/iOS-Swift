1340e9aa75d5cf0c28ef376099c2d877280ee94c
Alejandro Tovar <da.tovar10@uniandes.edu.co>
2023-12-01 16:09:38 -0500

    Claims Feature working!!

--- SportPal-iOS/Modules/Profile/Views/ClaimView.swift
+++ SportPal-iOS/Modules/Profile/Views/ClaimView.swift
@@ -16,84 +16,122 @@ struct ClaimView: View {
     @State private var claim: String = ""
     @State private var claims: [ClaimRequest]?
     
+    @StateObject var networkMonitor = NetworkMonitor()
+    
     var body: some View {
         VStack {
             HeaderBack(title: "NEW CLAIM") {
                 navPaths.removeLast()
             }
+            
             LazyVStack () {
-                VStack{
+                if(!networkMonitor.isActive) {
+                    Text("Claims functionality needs internet connection to function!")
+                        .font(.system(size: 20, design: .default))
+                        .fontWeight(.thin)
+                        .foregroundColor(Color.black)
+                } else {
+                    VStack{
+                        
+                        CustomTextField(
+                            placeholder: "Describe your claim...",
+                            text: $claim,
+                            isValid: profileViewModel.isValidClaim(claim: claim)
+                        ).padding(.horizontal)
+                        
+                        Button(action: {
+                            let claimRequest =  ClaimRequest(
+                                user_created_id: profileViewModel.getUser().id,
+                                content: claim
+                            )
+                            profileViewModel.sendClaim(claimData: claimRequest) {
+                                result in
+                                switch result {
+                                case true:
+                                    navPaths.append(.profile)
+                                case false: break;
+                                }
+                            }
+                        }) {
+                            Text("Send")
+                                .font(.title)
+                                .padding(.horizontal, 80)
+                                .padding(.vertical, 10)
+                        }
+                        .buttonStyle(CustomButtonStyle(isEnabled: profileViewModel.isValidClaim(claim: claim)))
+                        .padding(.horizontal)
+                        .padding(.vertical, 20)
+                        .disabled(!profileViewModel.isValidClaim(claim: claim))
+                    }
+                    .padding(.horizontal, 20)
+                    .padding(.top, 50.0)
                     
-                    CustomTextField(
-                        placeholder: "Describe your claim...",
-                        text: $claim,
-                        isValid: profileViewModel.isValidClaim(claim: claim)
-                    ).padding(.horizontal)
-                    
-                    Button(action: {
-                        let claimRequest =  ClaimRequest(
-                            user_created_id: profileViewModel.getUser().id,
-                            content: claim
-                        )
-                        profileViewModel.sendClaim(claimData: claimRequest) {
-                            result in
-                            switch result {
-                            case true:
-                                navPaths.append(.profile)
-                            case false: break;
+                    ScrollView{
+                        VStack {
+                            Text("User claims:")
+                            
+                            ForEach(0..<profileViewModel.getClaims().count, id: \.self) { index in
+                                ClaimDetailView(claim: profileViewModel.getClaims()[index])
                             }
                         }
-                    }) {
-                        Text("Send")
-                            .font(.title)
-                            .padding(.horizontal, 80)
-                            .padding(.vertical, 10)
                     }
-                    .buttonStyle(CustomButtonStyle(isEnabled: profileViewModel.isValidClaim(claim: claim)))
-                    .padding(.horizontal)
-                    .padding(.vertical, 20)
-                    .disabled(!profileViewModel.isValidClaim(claim: claim))
                 }
-                .padding(.horizontal, 20)
-                .padding(.top, 50.0)
-                
-                
-                VStack {
-                    Text("User claims:")
-                    ForEach(0..<profileViewModel.getClaims().count, id: \.self) { index in
-                            Text(profileViewModel.getClaims()[index].content)
-                        }
+                    
+                }.padding()
+                    .background(Color(red: 0.961, green: 0.961, blue: 0.961))
+                    .navigationBarBackButtonHidden(true)
+                    .alert(isPresented: $profileViewModel.showAlert) {
+                        Alert(title: Text(profileViewModel.alertTitle), message: Text(profileViewModel.errorMessage), dismissButton: .default(Text("OK")))
+                    }
+                Spacer()
+                FooterView(navPaths: $navPaths)
+            }.background(Color(red: 0.961, green: 0.961, blue: 0.961))
+                .onAppear() {
+                    print("VStack appeared")
+                    //                profileViewModel.fetchClaims() {
+                    //                    result in
+                    //                    switch result {
+                    //                    case true:
+                    //                        print("Claims fethed")
+                    //                    case false:
+                    //                        print("Claims failed")
+                    //                    }
+                    //                }
                 }
-                
+            
+        }
+    }
+    
+    struct ClaimView_Previews: PreviewProvider {
+        static var previews: some View {
+            ClaimView(navPaths: .constant([]))
+        }
+    }
+    
+    struct ClaimDetailView: View {
+        
+        var claim: Claim
+        
+        var body: some View {
+            HStack{
+                VStack(alignment: .center, spacing: 1 ){
+                    
+                    Text("User email: \(claim.user_created.email)")
+                        .font(.system(size: 20, design: .default))
+                        .fontWeight(.thin)
+                        .foregroundColor(Color.black)
+                    
+                    
+                    Text("Says: \(claim.content)")
+                        .font(.system(size: 20, design: .default))
+                        .fontWeight(.thin)
+                        .foregroundColor(Color.black)
+                    
+                }
                 
             }.padding()
-                .background(Color(red: 0.961, green: 0.961, blue: 0.961))
-                .navigationBarBackButtonHidden(true)
-                .alert(isPresented: $profileViewModel.showAlert) {
-                    Alert(title: Text(profileViewModel.alertTitle), message: Text(profileViewModel.errorMessage), dismissButton: .default(Text("OK")))
-                }
-            Spacer()
-            FooterView(navPaths: $navPaths)
-        }.background(Color(red: 0.961, green: 0.961, blue: 0.961))
-            .onAppear() {
-                print("VStack appeared")
-//                profileViewModel.fetchClaims() {
-//                    result in
-//                    switch result {
-//                    case true:
-//                        print("Claims fethed")
-//                    case false:
-//                        print("Claims failed")
-//                    }
-//                }
-            }
-        
+                .background(Color.white)
+                .cornerRadius(16)
+        }
     }
-}
-
-struct ClaimView_Previews: PreviewProvider {
-    static var previews: some View {
-        ClaimView(navPaths: .constant([]))
-    }
-}
-
+    
